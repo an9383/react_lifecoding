9c10d28fd8642eafed7f9f90124f2f4a52399b9c
an9383 <an9383@gmail.com>
2025-03-02 11:13:31 +0900

    all

--- source/react-app/src/App.js
+++ source/react-app/src/App.js
@@ -4,7 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello React!
+      <header>
+        <h1><a href="/">WEB</a></h1>
+      </header>
+      <nav>
+        <ol>
+          <li><a href="/read/1">html</a></li>
+          <li><a href="/read/2">css</a></li>
+          <li><a href="/read/3">js</a></li>
+        </ol>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, WEB
+      </article>
     </div>
   );
 }
